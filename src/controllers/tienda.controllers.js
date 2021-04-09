35632124e089ecaafd4203601265b69022953571
Diego <consultoresartificialbrain@gmail.com>
2021-04-09 15:05:09 -0500

    agregando todo

--- src/controllers/tienda.controllers.js
+++ src/controllers/tienda.controllers.js
@@ -129,9 +129,12 @@ tiendaCtrl.eliminarTienda = async (req, res) => {
 
 tiendaCtrl.cambiarColoresTienda = async (req,res) => {
     try {
-        
+        //const {navPrimary,navSecondary,bodyPage} = req.body;
+        await Tienda.findByIdAndUpdate(req.params.idTienda,req.body);
+        res.status(200).json({message: "Cambios realizados"});
     } catch (error) {
-        
+        res.status(500).json({message: 'Hubo un error al eliminar Tienda', error});
+        console.log(error);
     }
 }
 
